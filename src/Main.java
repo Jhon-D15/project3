3857bf0d9546dc73244fd31ee3db01876eedf98a
Jhon-D15 <149914828+Jhon-D15@users.noreply.github.com>
2023-11-05 23:47:53 -0500

    high numbers

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner lector =new Scanner(System.in);
+
+        double num;
+        double resultado;
+
+        System.out.println("ingresar el numero al que quiere elevar al cuadrado");
+        num= lector.nextDouble();
+
+
+        resultado = (num*num);
+
+        System.out.println("la suma de estos dos numeros es:"+" "+resultado);
+
+    }
+}
