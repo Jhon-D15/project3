3bf0002b48d67c1cb700d0886329b86ee4f640bc
Jhon-D15 <149914828+Jhon-D15@users.noreply.github.com>
2023-11-16 14:00:24 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class projec3 {
     public static void main(String[] args) {
 
         Scanner lector =new Scanner(System.in);
@@ -8,13 +8,15 @@ public class Main {
         double num;
         double resultado;
 
+        System.out.println("///////////////////////////////////////////////////");
+
         System.out.println("ingresar el numero al que quiere elevar al cuadrado");
         num= lector.nextDouble();
 
 
         resultado = (num*num);
 
-        System.out.println("la suma de estos dos numeros es:"+" "+resultado);
+       System.out.println("la suma de estos dos numeros es:"+" "+resultado);
 
     }
 }
